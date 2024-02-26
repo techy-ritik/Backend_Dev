2f4875cac3e580461ae253d55eed1bea13d1f11c
techy-ritik <ritikeshjee@gmail.com>
2024-02-26 19:43:55 +0530

    custom response code updated

--- app.js
+++ app.js
@@ -6,8 +6,33 @@ const http = require("http");
 // http.createServer(rqListener);
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
+  // console.log(req.url, req.method, req.headers);
 
+  const url= req.url;
+  if (url === "/home"){
+    res.write("<html>");
+    res.write("<head><title>my response</title></head>");
+    res.write("<body><h1>Welcome Home</h1></body>");
+    res.write("</html>");
+    return res.end();
+  }
+
+  else if (url === "/about") {
+    res.write("<html>");
+    res.write("<head><title>my response</title></head>");
+    res.write("<body><h1>Welcome to About Us Page</h1></body>");
+    res.write("</html>");
+    return res.end();
+  } 
+
+  else if (url === "/node") {
+    res.write("<html>");
+    res.write("<head><title>my response</title></head>");
+    res.write("<body><h1>Welcome to my Node Js project</h1></body>");
+    res.write("</html>");
+    return res.end();
+  } 
+  
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>my response</title></head>");
