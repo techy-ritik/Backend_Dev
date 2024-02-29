ea09a3a2a64e47078d4daac4bce6b7d1b5e420e8
techy-ritik <ritikeshjee@gmail.com>
2024-02-29 17:30:30 +0530

    write and read file functionality done

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const http = require("http");
+const fs = require("fs");
+const { buffer } = require("stream/consumers");
 
 // function rqListener(req,res){
 
@@ -8,37 +10,50 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   // console.log(req.url, req.method, req.headers);
 
-  const url= req.url;
-  if (url === "/home"){
-    res.write("<html>");
-    res.write("<head><title>my response</title></head>");
-    res.write("<body><h1>Welcome Home</h1></body>");
+  const url = req.url;
+  const method = req.method;
+
+
+  if (url === "/") {
+    fs.readFile("message.text", 'utf8' , (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      // res.setHeader("Content-Type", "text/html");
+      // console.log("data" , data);
+      res.write("<html>");
+      res.write("<head><title>my response</title></head>");
+      res.write(`<body>${data}</body>`);
+      res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
     res.write("</html>");
     return res.end();
+    });
+  }
+   else if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      // console.log(chunk);
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.text", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
   }
 
-  else if (url === "/about") {
-    res.write("<html>");
-    res.write("<head><title>my response</title></head>");
-    res.write("<body><h1>Welcome to About Us Page</h1></body>");
-    res.write("</html>");
-    return res.end();
-  } 
-
-  else if (url === "/node") {
-    res.write("<html>");
-    res.write("<head><title>my response</title></head>");
-    res.write("<body><h1>Welcome to my Node Js project</h1></body>");
-    res.write("</html>");
-    return res.end();
-  } 
-  
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>my response</title></head>");
-  res.write("<body><h1>Ritikesh Kumar</h1></body>");
-  res.write("</html>");
-  res.end();
+  // res.setHeader("Content-Type", "text/html");
+  // res.write("<html>");
+  // res.write("<head><title>my response</title></head>");
+  // res.write("<body><h1>Ritikesh Kumar</h1></body>");
+  // res.write("</html>");
+  // res.end();
   // process.exit();
 });
 
