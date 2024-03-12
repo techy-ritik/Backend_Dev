c67fd3ddf8d0e613477492d134dfec568d6aef2a
techy-ritik <ritikeshjee@gmail.com>
2024-03-13 02:26:41 +0530

    express js installed and middleware added

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 const http = require("http");
-const routs = require('./routs');
 
-console.log(routs.text);
-const server = http.createServer(routs.handler);
+const express = require('express');
 
-server.listen(4000);
+const app = express();
+
+app.use((req,res,next)=>{
+    console.log('in the first middleware');
+    next();  // Allows to move the request to the next middleware for continuing further code execution 
+})
+
+
+app.use((req, res, next) => {
+  console.log("in the second middleware");
+  res.send({ name: "Ritik" });
+});
+
+// const routs = require('./routs');
+
+// console.log(routs.text);
+// const server = http.createServer(routs.handler);
+
+
+// const server = http.createServer(app);
+
+// server.listen(4000);
+
+app.listen(3000);
