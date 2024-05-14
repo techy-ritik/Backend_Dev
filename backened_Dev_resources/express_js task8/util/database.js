717730446785e760429045ef33f295948f474b68
techy-ritik <ritikeshjee@gmail.com>
2024-05-15 01:28:42 +0530

    sequelize added to the server

--- backened_Dev_resources/express_js task8/util/database.js
+++ backened_Dev_resources/express_js task8/util/database.js
@@ -1,10 +1,21 @@
-const mysql=require('mysql2');
+// const mysql=require('mysql2');
 
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "sharpNode",
-  password: "ritikesh1113",
+// const pool = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   database: "sharpNode",
+//   password: "ritikesh1113",
+// });
+
+// module.exports=pool.promise();
+
+const Sequelize = require("sequelize");
+const { AbstractDialect } = require("sequelize/lib/dialects/abstract/index");
+
+const sequelize = new Sequelize("sharpnode", "root", "ritikesh1113", {
+  dialect: "mysql",
+  host: 'localhost',
 });
 
-module.exports=pool.promise();
+
+module.exports=sequelize;
